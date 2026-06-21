df4d4262303d199de82285eaaa437a59c5567455
MOHD ARSLAN <64858711+QUANTUM00007@users.noreply.github.com>
2026-06-22 00:56:56 +0530

    Add Admin Panel dashboard

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ShortURL
 
-# Register your models here.
+@admin.register(ShortURL)
+class ShortURLAdmin(admin.ModelAdmin):
+    list_display = (
+        "short_code",
+        "original_url",
+        "clicks",
+        "created_at"
+    )
+
+    search_fields = (
+        "short_code",
+        "original_url"
+    )
