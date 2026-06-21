2c4c48c31ba9d9cdaa2d4afc783243f765be33af
MOHD ARSLAN <64858711+QUANTUM00007@users.noreply.github.com>
2026-06-21 16:43:18 +0530

    Initial Django project setup

---
+++ shortener/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+import string
+import random
+
+
+def generate_code():
+    chars = string.ascii_letters + string.digits
+    return ''.join(random.choice(chars, k=6))
+
+
+class ShortURL(models.Model):
+    orginal_url = models.Models.URLField()
+    short_code = models.CharField(
+        max_length=10,
+        unique=True,
+        default=generate_code
+    )
+    clicks = models.PositiveBigIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.short_code
